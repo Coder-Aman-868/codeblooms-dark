166270b28133070b6e57100eaa793bf1fe104e03
Aman <developeraman868@gmail.com>
2026-06-02 14:17:11 +0530

    feat: add reusable Button component and update @react-three/fiber dependency

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const Button = ({ children, className, secondary = false, ref, type = "button", onClick }: Props) => {
     return (
-        <button ref={ref} type={type} onClick={onClick} className={`rounded-[999px] cursor-pointer group relative overflow-hidden md:py-6 py-4 sm:px-10 px-5.5 text-base font-medium  leading-none transition-all duration-300 ease-in-out ${secondary ? "border border-white/8 bg-white/5 hover:bg-white/2 hover:border-white/50 backdrop-blur-[22px] text-[#B8B8B8]" : "text-black bg-white hover:bg-transparent hover:text-white border border-transparent hover:border-white"} ${className}`}>
+        <button ref={ref} type={type} onClick={onClick} className={`rounded-[999px] cursor-pointer group relative overflow-hidden py-4 px-5.5 text-base font-medium  leading-none transition-all duration-300 ease-in-out ${secondary ? "border border-white/8 bg-white/5 hover:bg-white/2 hover:border-white/50 backdrop-blur-[22px] text-[#B8B8B8]" : "text-black bg-white hover:bg-transparent hover:text-white border border-transparent hover:border-white"} ${className}`}>
             {children}
         </button>
     )
