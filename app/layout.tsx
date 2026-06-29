4de7aaed770c896bd951a9ded400a9f7ff6a063c
Aman <developeraman868@gmail.com>
2026-06-29 10:43:39 +0530

    feat: initialize root layout with global SEO metadata and GTM tracking integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,9 +84,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("font-sans", inter.variable)}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPJ8QZ4K');`}} />
+      </head>
       <body
         className={`${poppins.className} antialiased bg-black`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPJ8QZ4K" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <Navbar />
         <main>
           {children}
